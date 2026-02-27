6223bd8159380e8af1ff0e9cb17cadb60839a724
yorichill <thomas.haenel101@gmail.com>
2026-02-27 14:43:49 +0100

    feat: implement facade logic for places and amenities management

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,17 +1,60 @@
 from app.persistence.repository import InMemoryRepository
+from app.models.user import User
+from app.models.amenity import Amenity
+from app.models.place import Place
+from app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        # On initialise les dépôts pour chaque entité
         self.user_repo = InMemoryRepository()
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
-    # Placeholder : Logique pour créer un utilisateur
-    def create_user(self, user_data):
-        pass
+    # méthode amentity
 
-    # Placeholder : Logique pour récupérer un lieu
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(name=amenity_data['name'])
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        self.amenity_repo.update(amenity_id, amenity_data)
+        return self.get_amenity(amenity_id)
+
+    # méthode place
+
+    def create_place(self, place_data):
+        owner_id = place_data.get('owner_id')
+        owner = self.user_repo.get(owner_id)
+        
+        if not owner:
+            raise ValueError("Le propriétaire spécifié n'existe pas.")
+
+        place = Place(
+            title=place_data['title'],
+            description=place_data.get('description', ''),
+            price=place_data['price'],
+            latitude=place_data['latitude'],
+            longitude=place_data['longitude'],
+            owner=owner
+        )
+        
+        self.place_repo.add(place)
+        return place
+
     def get_place(self, place_id):
-        pass
+        return self.place_repo.get(place_id)
+
+    def get_all_places(self):
+        return self.place_repo.get_all()
+
+    def update_place(self, place_id, place_data):
+        self.place_repo.update(place_id, place_data)
+        return self.get_place(place_id)
